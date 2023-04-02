df4b9c169d916318fb14c585f25940dd17a2a25b
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-04-02 16:57:08 +0300

    add Task002 Seminar 7

---
+++ Task002/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+
+
+void FillArray(int [,] array)
+{
+    for (int i = 0;i< array.GetLength(0);i++)
+    {
+        for (int j = 0;j<array.GetLength(0);j++ )
+        {
+            array[i,j] = i+j;
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i =0;i<array.GetLength(0);i++)
+    {
+        for (int j=0;j<array.GetLength(0);j++)
+        {
+            Console.Write($"{array[i,j],3} \t");
+        
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+
+Console.WriteLine("Введите колтичество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int [rows,cols];
+FillArray(array);
+PrintArray(array);
